6083764f20618de99715599b47c3b9e3ea7abf51
kimgeunhoe <ghkim1031@naver.com>
2022-08-17 14:12:52 +0900

    Create 산술연산자.java

---
+++ 01_Variable_Operator/src/operators/산술연산자.java
@@ -0,0 +1,14 @@
+package operators;
+
+public abstract class 산술연산자 {
+	//한글로 클래스명과 변수를 선언할 수 있으나 컴파일할떄 한번 더 인코딩과정이 진행되므로 사용하지 않음
+	public static void main(String[] args) {
+		System.out.println("사칙연산자: +, -, *, /");
+		System.out.println("나머지연산자: %");
+		System.out.println("증감연산자: ++, --");
+
+		System.out.println("대입연산자");
+		System.out.println("=, +=, -=, *=, /=, %=");
+	}
+
+}
