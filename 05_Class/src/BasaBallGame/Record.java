c1078c834a0efa48b04d00a578d16540a5c22494
kimgeunhoe <ghkim1031@naver.com>
2022-08-24 14:02:58 +0900

    Create Record.java

---
+++ 05_Class/src/BasaBallGame/Record.java
@@ -0,0 +1,14 @@
+package BasaBallGame;
+
+public class Record {
+	private User users;
+
+	public User getUsers() {
+		return users;
+	}
+
+	public void setUsers(User users) {
+		this.users = users;
+	}
+
+}
