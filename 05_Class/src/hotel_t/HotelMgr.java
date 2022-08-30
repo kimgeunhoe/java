3e8af5717224e4f157af7df6db70d0a0059910de
kimgeunhoe <ghkim1031@naver.com>
2022-08-30 14:24:38 +0900

    Create HotelMgr.java

---
+++ 05_Class/src/hotel_t/HotelMgr.java
@@ -0,0 +1,14 @@
+package hotel_t;
+
+public class HotelMgr {
+	// 과제 - 호텔예약 프로그램 만들기
+	// 제출처 : miaseye@naver.com
+	// 제출기한 : 2022-05-16 14:00 이전
+	// 제출파일명 : java_1st_work_이름_20220516.zip
+
+	public static void main(String[] args) {
+		Hotel hotel = new Hotel();
+		//Hotel hotel = new Hotel(15, 15);
+		hotel.goFrontDest();
+	}
+}
