1d6a91c86b86e3dc55853df791e2712a7b2b5159
kimgeunhoe <ghkim1031@naver.com>
2022-08-31 22:26:59 +0900

    Create Room.java

---
+++ 05_Class/src/hotel_t/Room.java
@@ -0,0 +1,141 @@
+package hotel_t;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Room {
+	private String roomNum;
+	private User user;
+	private boolean empty;
+	private String checkInTime;
+	private String checkOutTime;
+	Scanner sc = new Scanner(System.in);
+	
+	public Room() {
+		
+	}
+
+	public Room(String roomNum, User user, boolean empty, String checkInTime, String checkOutTime) {
+		super();
+		this.roomNum = roomNum;
+		this.user = user;
+		this.empty = empty;
+		this.checkInTime = checkInTime;
+		this.checkOutTime = checkOutTime;
+	}
+
+	public String getRoomNum() {
+		return roomNum;
+	}
+
+	public void setRoomNum(String roomNum) {
+		this.roomNum = roomNum;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public boolean isEmpty() {
+		return empty;
+	}
+
+	public void setEmpty(boolean empty) {
+		this.empty = empty;
+	}
+
+	public String getCheckInTime() {
+		return checkInTime;
+	}
+
+	public void setCheckInTime(String checkInTime) {
+		this.checkInTime = checkInTime;
+	}
+
+	public String getCheckOutTime() {
+		return checkOutTime;
+	}
+
+	public void setCheckOutTime(String checkOutTime) {
+		this.checkOutTime = checkOutTime;
+	}
+
+	@Override
+	public String toString() {
+		return "Room [roomNum=" + roomNum + ", empty=" + empty + ", checkInTime=" + checkInTime
+				+ ", checkOutTime=" + checkOutTime + "]";
+	}
+	
+//	public Room(int i, int j) {
+//		roomNum = (j < 9) ? Integer.toString(i+1) + "0" + Integer.toString(j+1): Integer.toString(i+1) + Integer.toString(j+1);
+//		empty = true;
+//	}
+//	
+//	void getRoomInfo() {
+//		String check = empty==true ? "빈방 " : "사용 중";
+//		System.out.print(roomNum + "호 " + check + "\t");
+//	}
+//
+//	String getTime() {
+//		SimpleDateFormat time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//		return time.format(new Date());
+//	}
+//	
+//	void checkIn() {
+//		if(empty==false) {
+//			System.out.println("이미 사용 중인 방입니다.");
+//		}
+//		else {
+//			//user = new User();
+//			System.out.println("사용자의 신상");
+//			System.out.print("이름: ");
+//			String name = sc.next();
+//			System.out.print("나이: ");
+//			int age = sc.nextInt();
+//			System.out.print("성별(M/W): ");
+//			char gen = sc.next().charAt(0);
+//			user.setUserInfo(name, age, gen);
+//			empty = false;
+//			checkInTime = getTime();
+//			checkOutTime = "";
+//			System.out.println(roomNum + "호 입실이 완료되었습니다.");
+//		}
+//	}
+//	
+//	void checkOut() {
+//		if(empty==true) {
+//			System.out.println("이미 빈 방입니다.");
+//		}
+//		else {
+//			System.out.println(roomNum + "호 퇴실이 완료되었습니다.");
+//			user.delUserInfo();
+//			empty = true;
+//			checkInTime = "";
+//			checkOutTime = getTime();
+//		}
+//	}
+//	
+//	void getRoomInfoDetail() {
+//		System.out.print("방정보\n" + roomNum + "호. ");
+//		if(empty==true) {
+//			if(checkOutTime==null) {
+//				System.out.println("사용이력이 없습니다.");
+//			} 
+//			else {
+//				System.out.println("빈 방. 퇴실시간: " + checkOutTime);
+//			}
+//		}
+//		else {
+//			System.out.println("사용 중. 입실시간: " + checkInTime);
+//		}
+//	}
+//	
+//	void getUserInfo() {
+//		user.getUserInfo();
+//	}
+}
