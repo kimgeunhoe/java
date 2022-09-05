d4ee67ecfbe3bf7d813e2381bdecfd3677bf36c9
kimgeunhoe <ghkim1031@naver.com>
2022-09-05 12:09:57 +0900

    Create Staff.java

---
+++ 07_Inheritance/src/basic/Staff.java
@@ -0,0 +1,22 @@
+package basic;
+
+public class Staff extends Person{
+	private String job;
+	
+	public Staff() {
+		System.out.println("This is Staff Instance");
+	}
+	
+	public void printjob() {
+		System.out.println(super.getName() + "직원의 업무는" + job);
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	public void setJob(String job) {
+		this.job = job;
+	}
+	
+}
