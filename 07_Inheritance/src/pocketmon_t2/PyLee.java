e2d1b554fd3477d4c25276b9170a0a9ae0157579
kimgeunhoe <ghkim1031@naver.com>
2022-09-07 14:56:03 +0900

    Create PyLee.java

---
+++ 07_Inheritance/src/pocketmon_t2/PyLee.java
@@ -0,0 +1,35 @@
+package pocketmon_t2;
+
+public class PyLee extends Character {
+
+	@Override
+	public void eat() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sleep() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void play() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean train() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void levelUp() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
