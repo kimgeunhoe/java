69db98d44c5a04c00c83ba14bbbc47533c61b369
kimgeunhoe <ghkim1031@naver.com>
2022-09-19 20:07:47 +0900

    Create MapEx.java

---
+++ 10_Collection_Framework/src/map/MapEx.java
@@ -0,0 +1,40 @@
+package map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class MapEx {
+
+	public static void main(String[] args) {
+		Map map = new HashMap();
+		
+		map.put("lee", "1234");
+		map.put("kim", "1236");
+		map.put("cho", "1239");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		while(true) {
+			System.out.println("id와 pw를 입력해주세요");
+			System.out.println("id:");
+			String id = sc.next();
+			System.out.println("pw:");
+			String pw = sc.next();
+			
+			if(map.containsKey(id)) {
+				if(map.get(id).equals(pw)) {
+					System.out.println("로그인 성공");
+				}
+				else {
+					System.out.println("비밀번호 다름");
+				}
+			}
+			else {
+				System.out.println("아이디 없음");
+				break;
+			}
+		}
+	}
+
+}
