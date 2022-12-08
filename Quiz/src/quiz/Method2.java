e035001f0010d0567a1f95ff1d605d3be33c1cea
kimgeunhoe <ghkim1031@naver.com>
2022-12-08 21:56:39 +0900

    Create Method2.java

---
+++ Quiz/src/quiz/Method2.java
@@ -0,0 +1,13 @@
+package quiz;
+
+public class Method2 {
+
+	public static void main(String[] args) {
+		compute("apple", 10, 1200);
+	}
+
+	private static void compute(String string, int i, int j) {
+		System.out.println(string);
+		System.out.println(i*j + "\n");
+	}
+}
